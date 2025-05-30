72cb5dbb25b8fe0f02d8335fb9f63a044de93de9
Viktor <v-henriksson@hotmail.com>
2025-05-30 15:42:34 +0200

    Added testing

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,34 @@ pub mod x5_254_3 {
     }
     
     pub fn hash(input: &[ark_bn254::Fr]) -> [ark_bn254::Fr; 1] {
+        // We probably want something other than all zeroes as the initial state.
+        // However, I have not yet found any reference value, so all zeroes will be
+        // used as a placeholder for now.
         let mut sponge = Sponge::<Fr, Perm_x5_254_3, 1, 3>::new([Fr::zero(); 3]);
         for i in input {
             sponge.absorb(&[*i]);
         }
         sponge.squeeze()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fr;
+    use ark_ff::{Zero, One};
+
+    #[test]
+    fn test_hash() {
+        let input_1 = vec![Fr::from(1), Fr::from(2), Fr::from(3)];
+        let result_1 = x5_254_3::hash(&input_1);
+        let input_2 = vec![Fr::from(1), Fr::from(2), Fr::from(3), Fr::zero()];
+        let result_2 = x5_254_3::hash(&input_2);
+        let input_3 = vec![Fr::from(1), Fr::from(2), Fr::from(3)];
+        let result_3 = x5_254_3::hash(&input_1);
+        // Since I have not found any reference values yet, I will be happy with asserting
+        // that equal inputs yield equal outputs, and different inputs yield different outputs.
+        assert_ne!(result_1, result_2);
+        assert_eq!(result_1, result_3);
     }
 }
