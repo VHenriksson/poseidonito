e3776a467ef52d10737573aa342cc3b3cb6f9d2c
Viktor <v-henriksson@hotmail.com>
2025-05-30 15:32:44 +0200

    Some refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,17 @@ pub mod x5_254_3 {
     use ark_bn254::Fr;
     use crate::configurations::config_x5_254_3::ConfigX5_254_3;
     use crate::permutation::perm;
+    use crate::sponge::{Permutation, Sponge};
 
-    struct Hades;
-    impl crate::sponge::Permutation<Fr, 3> for Hades {
+    struct Perm_x5_254_3;
+    impl Permutation<Fr, 3> for Perm_x5_254_3 {
         fn apply(state: &mut [Fr; 3]) {
             perm::<Fr, 3, ConfigX5_254_3>(state);
         }
     }
     
     pub fn hash(input: &[ark_bn254::Fr]) -> [ark_bn254::Fr; 1] {
-        let mut sponge = crate::sponge::Sponge::<ark_bn254::Fr, Hades, 1, 3>::new([ark_bn254::Fr::zero(); 3]);
+        let mut sponge = Sponge::<Fr, Perm_x5_254_3, 1, 3>::new([Fr::zero(); 3]);
         for i in input {
             sponge.absorb(&[*i]);
         }
